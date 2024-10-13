0a0c3dee0ef9aee9dea7246c44323c8911c763ac
Vitaly Simakov <vitaly.s.simakov@tele2.ru>
2024-10-13 18:05:10 +0500

    HW2: сделано все синхронно

--- part_2_echo_process/test/part2_echo_process_test.exs
+++ part_2_echo_process/test/part2_echo_process_test.exs
@@ -5,14 +5,9 @@ defmodule Part2EchoTest do
   test "got message" do
     expected_message = {:pong, :nonode@nohost}
 
-    {:ok, sender_pid} = Part2Echo.start_link()
-    listener_pid = Part2Echo.Listener.run(sender_pid)
+    Part2Echo.start_link()
+    result = Part2Echo.ping()
 
-    Part2Echo.push(listener_pid, :ping)
-    Process.sleep(400)
-
-    message_from_listener = Part2Echo.pop(sender_pid)
-
-    assert message_from_listener == expected_message
+    assert result == expected_message
   end
 end
