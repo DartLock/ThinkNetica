cb2fc71910292b4a72061f407a0207293838e4df
Vitaly Simakov <vitaly.s.simakov@tele2.ru>
2024-10-13 18:05:10 +0500

    HW2: сделано все синхронно

--- part_2_echo_process/lib/part2_echo.ex
+++ part_2_echo_process/lib/part2_echo.ex
@@ -1,79 +1,19 @@
 defmodule Part2Echo do
   use GenServer
 
-  @moduledoc """
-  Part2Echo.
-
-  - Используется своего рода отладочная информация через модуль Part2Echo.Debug,
-    что бы следить за ходом выполнения передачи и получения сообщений.
-
-  Основной процесс. Процесс передает сообщения слушателю, а так же принимает от слушателя ответы.
-
-  Основной процесс должен начинаться с вызова `start_link()`:
-  ```
-  {:ok, sender_pid} = Part2Echo.start_link()
-  ```
-
-  `sender_pid` используется для передачи слушателю в `Part2Echo.Listener.run(sender_pid)`
-  """
-
-  @doc """
-  Функция запускает основной процесс и возвращает кортеж вида `{:ok, pid}`.
-
-  ## Examples
-      {:ok, sender_pid} = Part2Echo.start_link()
-  """
-
   def start_link() do
-    GenServer.start_link(__MODULE__, "")
+    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
-  @doc """
-  Функция асинхронно передает сообщение слушателю.
-
-  - В качестве первого аргумента нужно передать PID слушателя.
-  - PID слушателя возвращается при вызове `listener_pid = Part2Echo.Listener.run(sender_pid)`
-
-  - Вторым аргументом передается сообщение например "Ping!"
-
-  ## Examples
-      Part2Echo.push(listener_pid)
-  """
-  def push(pid, message \\ :ping) do
-    GenServer.cast(pid, {:push, message})
+  def init(:ok) do
+    {:ok, %{}}
   end
 
-  @doc """
-  Функция синхронно получает ответ от слушателя `Part2Echo.Listener`.
-
-  - В качестве первого аргумента нужно передать PID основного процесса.
-  - Возвращает ответ от слушателя, после запроса через функцию `push`.
-
-  ## Examples
-      response = Part2Echo.pop(sender_pid)
-  """
-  def pop(pid) do
-    GenServer.call(pid, :pop)
+  def handle_call(:ping, _from, state) do
+    {:reply, {:pong, node()}, state}
   end
 
-  # server
-
-  @impl true
-  def init(state) do
-    elements = String.split(state, ";", trim: true)
-    {:ok, elements}
-  end
-
-  @impl true
-  def handle_cast({:push, element}, state) do
-    new_state = [element | state]
-    {:noreply, new_state}
-  end
-
-  @impl true
-  def handle_call(:pop, _from, state) do
-    [to_caller | new_state] = state
-
-    {:reply, to_caller, new_state}
+  def ping do
+    GenServer.call(__MODULE__, :ping)
   end
 end
