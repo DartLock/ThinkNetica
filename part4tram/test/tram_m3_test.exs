05d727398a4d0e5ec7de71e40acb3325bc93cd21
Vitaly Simakov <vitaly.s.simakov@tele2.ru>
2024-10-25 13:37:47 +0500

    HW 4: первый рабочий тест

--- part4tram/test/tram_m3_test.exs
+++ part4tram/test/tram_m3_test.exs
@@ -5,13 +5,20 @@ defmodule TramM3Test do
   alias TramM3.Debug
 
   test "greets the world" do
-    assert {:ok, pid} = TramM3.start()
+    assert {:ok, pid} = TramM3.start(:first_street)
 
-    Debug.show("TEST #1", [{"pid", pid}])
+    # Debug.show("TEST #1", [{"pid", pid}])
 
-    state = TramM3.current(pid)
+    current_state = TramM3.current(pid)
 
-    Debug.show("TEST #2", [{"state", state}])
+    {current_street, state} = current_state
+
+    Debug.show("TEST #2", [{"current_state", current_state}, {"current_street", current_street}])
+
+    state_info = TramM3.get_tram_state_info(state)
+    street_name = TramM3.get_tram_stop_name(current_street)
+
+    Debug.show("TEST #3", [{"state", state}, {"state_info", state_info}, {"street_name", street_name}])
 
     # assert Tram.hello() == :world
   end
