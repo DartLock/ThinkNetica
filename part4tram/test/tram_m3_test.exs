0b0a8423c2fd0cf16bf2a06d20527d64259a001e
Vitaly Simakov <vitaly.s.simakov@tele2.ru>
2024-11-01 17:54:09 +0500

    HW 4: удален лишний стейт

--- part4tram/test/tram_m3_test.exs
+++ part4tram/test/tram_m3_test.exs
@@ -1,50 +1,36 @@
-defmodule TramM3Test do
+defmodule TramTest do
   use ExUnit.Case
 
   test "next state transtition to next stop" do
-    TramM3.start(:first_street)
+    Tram.start()
 
-    assert {:first_street, :stopped} == TramM3.current()
+    assert :stopped == Tram.current()
 
-    TramM3.next()
-    assert {:first_street, :doors_opened} == TramM3.current()
+    Tram.next()
+    assert :doors_opened == Tram.current()
 
-    TramM3.next()
-    assert {:first_street, :doors_closed} == TramM3.current()
+    Tram.next()
+    assert :doors_closed == Tram.current()
 
-    TramM3.next()
-    assert {:first_street, :driving} == TramM3.current()
+    Tram.next()
+    assert :driving == Tram.current()
 
-    TramM3.next()
-    assert {:second_street, :stopped} == TramM3.current()
-  end
-
-  test "get street name" do
-    street_name = TramM3.tram_stop_name(:first_street)
-    streets = TramM3.get_tram_stops()
-
-    assert street_name == streets[:first_street]
-  end
-
-  test "get tram stop info" do
-    state_info = TramM3.tram_state_info(:stopped)
-    states = TramM3.get_states()
-
-    assert state_info == states[:stopped]
+    Tram.next()
+    assert :stopped == Tram.current()
   end
 
   test "error when tram process is terminated" do
-    TramM3.start(:first_street)
-    TramM3.stop()
-    TramM3.next()
+    Tram.start()
+    Tram.stop()
+    Tram.next()
 
-    assert TramM3.current() == {:error, :terminated}
+    assert Tram.current() == :terminated
   end
 
   test "tram in terminating state" do
-    TramM3.start(:first_street)
-    TramM3.stop()
+    Tram.start()
+    Tram.stop()
 
-    assert TramM3.current() == {:terminating}
+    assert Tram.current() == :terminating
   end
 end
